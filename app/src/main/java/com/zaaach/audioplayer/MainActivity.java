11a19e0e9da4eab29e6811828a54a41530a04565
Zaaach <zaaach@aliyun.com>
2020-12-01 22:58:24 +0800

    * update sample

--- app/src/main/java/com/zaaach/audioplayer/MainActivity.java
+++ app/src/main/java/com/zaaach/audioplayer/MainActivity.java
@@ -1,18 +1,19 @@
 package com.zaaach.audioplayer;
 
-import android.Manifest;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zaaach.audioplayerhelper.AudioPlayerHelper;
 import com.zaaach.audioplayerhelper.OnAudioPlayStateChangeListener;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnAudioPlayStateChangeListener {
     private static final String MUSIC_URL = "http://music.163.com/song/media/outer/url?id=574633384.mp3";
@@ -21,21 +22,20 @@ public class MainActivity extends AppCompatActivity implements OnAudioPlayStateC
     private AudioPlayerHelper playerHelper;
     private Button playBtn;
     private SeekBar seekBar;
+    private TextView tvTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-        }
+        tvTimer = findViewById(R.id.tv_timer);
         seekBar = findViewById(R.id.seek_bar);
         playBtn = findViewById(R.id.btn_play_or_pause);
+
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playerHelper.setDataSource(MUSIC_URL_2);
                 playerHelper.playOrPause();
             }
         });
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements OnAudioPlayStateC
                 .attachSeekBar(seekBar)
                 .setLooping(true)
                 .setDebug(true)
+                .setDataSource(MUSIC_URL_2)
                 .setOnAudioPlayStateChangeListener(this);
     }
 
@@ -71,6 +72,20 @@ public class MainActivity extends AppCompatActivity implements OnAudioPlayStateC
         playerHelper.destroy();
     }
 
+    public String formatSeconds(long seconds){
+        String standardTime;
+        if (seconds <= 0){
+            standardTime = "00:00";
+        } else if (seconds < 60) {
+            standardTime = String.format(Locale.getDefault(), "00:%02d", seconds % 60);
+        } else if (seconds < 3600) {
+            standardTime = String.format(Locale.getDefault(), "%02d:%02d", seconds / 60, seconds % 60);
+        } else {
+            standardTime = String.format(Locale.getDefault(), "%02d:%02d:%02d", seconds / 3600, seconds % 3600 / 60, seconds % 60);
+        }
+        return standardTime;
+    }
+
     //=================================
     // 播放器回调
     //=================================
@@ -81,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements OnAudioPlayStateC
     @Override
     public void onPrepared(MediaPlayer player, long duration) {
         seekBar.setMax((int) duration);
+        tvTimer.setText("00:00 / " + formatSeconds(duration/1000));
     }
 
     @Override
